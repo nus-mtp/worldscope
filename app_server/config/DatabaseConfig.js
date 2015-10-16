ec2e765fa82cbd30f565ce9f2ce1dfbdb80b31cc
sharminelow <slxmlow@gmail.com>
2015-10-17 02:31:05 +0800

    Changes after review, shifted config file out

---
+++ app_server/config/DatabaseConfig.js
@@ -0,0 +1,13 @@
+/*
+ * A template config file. Edit to your local mysql database's credentials
+ */
+
+var config = {
+  name: '',
+  username: '',
+  password: '',
+  host: 'localhost',
+  dialect: 'mysql'
+};
+
+module.exports = config;
