c01faa7c12edb57886044525c015a90b9b26e9ca
a0110674 <a0110674@u.nus.edu>
2015-10-16 23:14:59 +0800

    Basic home view implemented with button to go to record state

--- client/android/WorldScope/app/src/main/java/com/litmus/worldscope/MainActivity.java
+++ client/android/WorldScope/app/src/main/java/com/litmus/worldscope/MainActivity.java
@@ -1,8 +1,8 @@
 package com.litmus.worldscope;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,13 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Redirect to RecordActivity
+                redirectToRecordActivity();
             }
-        });
+        }));
+    }
+
+    private void redirectToRecordActivity() {
+        Intent intent = new Intent(this, RecordActivity.class);
+        startActivity(intent);
     }
 
 }
