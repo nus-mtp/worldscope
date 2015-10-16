24bf57ec27dd61a0fc4b0d67c5732798819cb820
a0110674 <a0110674@u.nus.edu>
2015-10-16 23:13:32 +0800

    Dummy login landing state implemented

---
+++ client/android/WorldScope/app/src/main/java/com/litmus/worldscope/RecordActivity.java
@@ -0,0 +1,13 @@
+package com.litmus.worldscope;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
