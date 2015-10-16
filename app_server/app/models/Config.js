4ebc235e60e08b7d33aa55334bf7b2152139aea7
sharmine <slxmlow@gmail.com>
2015-10-16 16:06:26 +0800

    Setup database

---
+++ app_server/app/models/Config.js
@@ -0,0 +1,13 @@
+/*
+ * Edit to your local mysql database's credentials
+ */
+
+var config = {};
+
+config.name = '';
+config.username = '';
+config.password = '';
+config.host = 'localhost';
+config.dialect = 'mysql';
+
+module.exports = config;
