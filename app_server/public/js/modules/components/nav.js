209b07013c86f8204b8cf5eda102868b6bf85daf
yj-soh <soh.you.jun@gmail.com>
2015-10-17 18:56:01 +0800

    Add hierarchical nav items

--- app_server/public/js/modules/components/nav.js
+++ app_server/public/js/modules/components/nav.js
@@ -1,3 +1,41 @@
 const m = require('mithril');
 
 const Nav = module.exports = {};
+
+Nav.navItems = [
+  {
+    'name': 'Metrics', 'href': '/metrics',
+    'sub': [
+      {'name': 'Overview', 'href': '/metrics/overview'},
+      {'name': 'Real-Time', 'href': '/metrics/realtime'},
+      {'name': 'Demographics', 'href': '/metrics/demographics'}
+    ]
+  },
+  {
+    'name': 'Streams', 'href': '/streams',
+    'sub': [
+      {'name': 'Live Streams', 'href': '/streams/live'},
+      {'name': 'All Streams', 'href': '/streams/all'},
+      {'name': 'Search', 'href': '/streams/search'}
+    ]
+  },
+  {
+    'name': 'Users', 'href': '/users',
+    'sub': [
+      {'name': 'All Users', 'href': '/users/all'},
+      {'name': 'Search', 'href': '/users/search'}
+    ]
+  },
+  {
+    'name': 'Admins', 'href': '/admins',
+    'sub': [
+      {'name': 'All Admins', 'href': '/admins/all'},
+      {'name': 'Create Admin', 'href': '/admins/create'}
+    ]
+  },
+  {
+    'name': 'Settings',
+    'href': '/settings',
+    'sub': []
+  }
+];
