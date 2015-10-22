50769e420bd181183c9bbd25d013bdccc7939efb
yj-soh <soh.you.jun@gmail.com>
2015-10-22 20:54:53 +0800

    Add css classes to nav and content

--- app_server/public/js/modules/app.js
+++ app_server/public/js/modules/app.js
@@ -4,9 +4,11 @@ const nav = require('./components/nav');
 const mixinPage = function (nav, page) {
   return {
     view: function() {
-      return m('div', {id: 'container'}, [
-        m('div', {id: 'nav'}, nav),
-        m('div', {id: 'content'}, page)
+      return m('div', {id: 'container', class: 'row'}, [
+        m('div', {id: 'nav', class: 'col s2 l1'},
+            nav),
+        m('div', {id: 'content', class: 'col offset-s2 s10 offset-l1 l11'},
+            page)
       ]);
     }
   };
