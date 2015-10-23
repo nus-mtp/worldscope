d118417fd05f98d6803332baa0ece29ab65639f8
YJSoh <soh.you.jun@gmail.com>
2015-10-23 19:26:04 +0800

    Implement pagination for future table (streams)

--- app_server/public/js/modules/pages/streams.js
+++ app_server/public/js/modules/pages/streams.js
@@ -1,9 +1,35 @@
 const m = require('mithril');
 
 const Streams = module.exports = {
-  controller: () => {
+  controller: function () {
+    this.currentPage = m.prop(1);
   },
   view: function (ctrl) {
-    return m('h1', 'Streams');
+    let getPageRange = function (curPage, totalLength) {
+      // TODO: Implement proper page range generation
+      //   Pass in possible items count, divide by items per page
+      //   Insert "..." if more than total length
+      let range = [];
+      let startPage = Math.max(curPage - Math.floor(totalLength / 2), 1);
+      for (let i = 0; i < totalLength; i++) {
+        range.push(startPage + i);
+      }
+      return range;
+    };
+
+    return [m('h1', 'Streams'),
+      m('div', {className: 'row right-align'}, [
+        m('div', {className: 'col s12'}, [
+          m('ul', {className: 'pagination'}, [
+            m('li', [m('a', '<')]),
+            getPageRange(ctrl.currentPage(), 5).
+                map((page) => ctrl.currentPage() == page ?
+                    m('li', {class: 'active'}, [m('a', page)]) :
+                    m('li', [m('a', page)])),
+            m('li', [m('a', '>')])
+          ])
+        ])
+      ])
+    ];
   }
 };
