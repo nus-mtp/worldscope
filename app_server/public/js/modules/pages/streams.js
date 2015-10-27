a5c861cda9c3af3cbac2c277d8c7afc8cd338bed
YJSoh <soh.you.jun@gmail.com>
2015-10-27 12:46:23 +0800

    Display streams in table

--- app_server/public/js/modules/pages/streams.js
+++ app_server/public/js/modules/pages/streams.js
@@ -1,13 +1,19 @@
 const m = require('mithril');
 
 const getData = function () {
+  let formatStats = (viewers, stickers) => [
+    m('span', viewers + 'V'),
+    m('br'),
+    m('span', stickers + 'S')
+  ];
+
   return m.request({method: 'GET', url: 'js/modules/mockdata/streams.json'}).
       then((streams) => streams.map(
           function (stream) {
             return {
               title: stream.title,
               desc: stream.description,
-              stats: stream.totalViewers + 'V ' + stream.totalStickers + 'S',
+              stats: formatStats(stream.totalViewers, stream.totalStickers),
               date: stream.startDateTime,
               user: stream.user.alias
             };
@@ -17,6 +23,21 @@ const getData = function () {
 
 const Streams = module.exports = {
   controller: function () {
+    let table = {
+      columns: ['title', 'desc', 'stats', 'date', 'user'],
+      names: {
+        title: 'Title',
+        desc: 'Description',
+        stats: 'Statistics',
+        date: 'Start Date',
+        user: 'User'
+      },
+      data: getData()
+    };
+    this.table = m.prop(table);
+    this.columns = m.prop(table.columns);
+    this.names = m.prop(table.names);
+    this.data = table.data;
     this.currentPage = m.prop(1);
   },
   view: function (ctrl) {
@@ -43,6 +64,20 @@ const Streams = module.exports = {
                     m('li', [m('a', page)])),
             m('li', [m('a', '>')])
           ])
+        ])
+      ]),
+      m('table', {className: 'bordered responsive-table'}, [
+        m('thead', [
+          m('tr', [
+            ctrl.columns().map((col) => m('td', ctrl.names()[col]))
+          ])
+        ]),
+        m('tbody', [
+          ctrl.data().map(function (stream) {
+            return m('tr', [
+              ctrl.columns().map((col) => m('td', stream[col]))
+            ]);
+          })
         ])
       ])
     ];
