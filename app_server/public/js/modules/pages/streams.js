1ead9669690a37dae618c2cbda173147b3060a5f
YJSoh <soh.you.jun@gmail.com>
2015-10-30 15:30:29 +0800

    Refactor pagination view and add links

--- app_server/public/js/modules/pages/streams.js
+++ app_server/public/js/modules/pages/streams.js
@@ -52,29 +52,35 @@ const Streams = module.exports = {
     ctrl.update();
   },
   view: function (ctrl) {
-    let getPageRange = function (curPage, totalLength) {
-      // TODO: Implement proper page range generation
-      //   Pass in possible items count, divide by items per page
-      //   Insert "..." if more than total length
-      let range = [];
-      let startPage = Math.max(curPage - Math.floor(totalLength / 2), 1);
-      for (let i = 0; i < totalLength; i++) {
-        range.push(startPage + i);
-      }
-      return range;
+    let getPagination = function () {
+      let getPageRange = function (curPage, totalLength) {
+        // TODO: Implement proper page range generation
+        //   Pass in possible items count, divide by items per page
+        //   Insert "..." if more than total length
+        let range = [];
+        let startPage = Math.max(curPage - Math.floor(totalLength / 2), 1);
+        for (let i = 0; i < totalLength; i++) {
+          range.push(startPage + i);
+        }
+        return range;
+      };
+
+      let getPageLink = (page) =>
+          m('a', {href: '?page=' + page, config: m.route}, page);
+
+      return [
+        m('li', [m('a', '<')]),
+        getPageRange(ctrl.currentPage(), 5).
+            map((page) => ctrl.currentPage() == page ?
+                m('li', {class: 'active'}, getPageLink(page)) :
+                m('li', getPageLink(page))),
+        m('li', [m('a', '>')])
+      ];
     };
 
     return [m('h1', 'Streams'),
       m('div', {className: 'row right-align'}, [
         m('div', {className: 'col s12'}, [
-          m('ul', {className: 'pagination'}, [
-            m('li', [m('a', '<')]),
-            getPageRange(ctrl.currentPage(), 5).
-                map((page) => ctrl.currentPage() == page ?
-                    m('li', {class: 'active'}, [m('a', page)]) :
-                    m('li', [m('a', page)])),
-            m('li', [m('a', '>')])
-          ])
         ])
       ]),
       m('table', {className: 'bordered responsive-table'}, [
