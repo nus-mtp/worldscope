d666f72b900d2d57f2fad897d504eb8803ef2297
a0110674 <a0110674@u.nus.edu>
2015-11-01 18:18:51 +0800

    Changed tabs header in MainActivity to the same one as the mock up

--- client/android/WorldScope/app/src/main/java/com/litmus/worldscope/MainActivity.java
+++ client/android/WorldScope/app/src/main/java/com/litmus/worldscope/MainActivity.java
@@ -128,11 +128,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Popular";
                 case 1:
-                    return "SECTION 2";
+                    return "Latest";
                 case 2:
-                    return "SECTION 3";
+                    return "Friends";
             }
             return null;
         }
