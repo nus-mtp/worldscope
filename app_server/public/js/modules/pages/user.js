45278cd106f5b43225f8ca0b5e1ba488ed5e81be
YJSoh <soh.you.jun@gmail.com>
2016-01-12 18:22:23 +0800

    Clarify makeInput function

--- app_server/public/js/modules/pages/user.js
+++ app_server/public/js/modules/pages/user.js
@@ -14,7 +14,7 @@ const User = module.exports = {
     ctrl.update = () => UserModel.update(ctrl.user());
   },
   view: function (ctrl) {
-    let makeInput = function (id, type, label, prop, options) {
+    let getLabelledInput = function (label, id, type, prop, options) {
       let inputType = options && options.className === 'materialize-textarea' ?
           'textarea' : 'input';
 
@@ -35,20 +35,21 @@ const User = module.exports = {
     };
 
     let user = ctrl.user();
-
     return [
       m('h1', 'Edit User'),
       m('div', {className: 'input-field col s12'},
-          makeInput('username', 'text', 'Username', user.username, {disabled: true})
+          getLabelledInput('Username', 'username', 'text', user.username,
+              {disabled: true})
       ),
       m('div', {className: 'input-field col s12'},
-          makeInput('alias', 'text', 'Alias', user.alias)
+          getLabelledInput('Alias', 'alias', 'text', user.alias)
       ),
       m('div', {className: 'input-field col s12'},
-          makeInput('email', 'text', 'Email', user.email)
+          getLabelledInput('Email', 'email', 'text', user.email)
       ),
       m('div', {className: 'input-field col s12'},
-          makeInput('desc', 'text', 'Description', user.description, {className: 'materialize-textarea'}, true)
+          getLabelledInput('Description', 'desc', 'text', user.description,
+              {className: 'materialize-textarea'})
       ),
       m('div', {className: 'col s12'},
           m('button', {className: 'btn', onclick: ctrl.update}, 'Submit')
