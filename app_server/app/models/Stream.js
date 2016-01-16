a9b8ad09b64d997d4aa98256079bcd0f5bcf071e
sharmine <slxmlow@gmail.com>
2016-01-17 00:40:16 +0800

    Add Stream schema outline

---
+++ app_server/app/models/Stream.js
@@ -0,0 +1,60 @@
+/*
+ * Stream is a sequelize object
+ * @module Stream
+ */
+
+module.exports = function(sequelize, DataTypes){
+  var Stream = sequelize.define('Stream', {
+    streamId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    appInstance: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    roomId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    totalStickers: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    totalViewers: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    live: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+    duration: {
+      type: DataTypes.STRING,
+      defaultValue: '0'
+    },
+    endedAt: {
+      type: DataTypes.DATE
+    },
+    description: {
+      type: DataTypes.TEXT
+    }
+  }, {
+    paranoid: true,
+    freezeTableName: true,
+    setterMethods: {
+      createdAt: function(newDate) {
+        this.setDataValue('createdAt', newDate);
+      }
+    }
+  });
+  return Stream;
+};
