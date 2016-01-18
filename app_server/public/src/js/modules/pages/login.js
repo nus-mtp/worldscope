097b2e0c27414e212dfda2adf564e362708c82d4
YJSoh <soh.you.jun@gmail.com>
2016-01-18 14:43:17 +0800

    Add login page view

---
+++ app_server/public/src/js/modules/pages/login.js
@@ -0,0 +1,20 @@
+const m = require('mithril');
+
+const Login = module.exports = {
+  controller: function () {},
+  view: function (ctrl) {
+    return m('div.row', [
+      m('form.col offset-s3 s6', [
+        m('div.input-field col s12', [
+          m('input#username', {type: 'text'}),
+          m('label', {for: 'username'}, 'Username')
+        ]),
+        m('div.input-field col s12', [
+          m('input#password', {type: 'password'}),
+          m('label', {for: 'password'}, 'Password')
+        ]),
+        m('button.btn col s12', {type: 'submit'}, 'Log In')
+      ])
+    ]);
+  }
+};
