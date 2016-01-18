42ec17758da48c2dea1ba80f3d62eecaebd5bf87
YJSoh <soh.you.jun@gmail.com>
2016-01-18 14:50:35 +0800

    Use MatCSS text initialization

--- app_server/public/src/js/modules/pages/login.js
+++ app_server/public/src/js/modules/pages/login.js
@@ -1,9 +1,11 @@
 const m = require('mithril');
 
+const mz = require('../utils/mzInit');
+
 const Login = module.exports = {
   controller: function () {},
   view: function (ctrl) {
-    return m('div.row', [
+    return m('div.row', mz.text, [
       m('form.col offset-s3 s6', [
         m('div.input-field col s12', [
           m('input#username', {type: 'text'}),
