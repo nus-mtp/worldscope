faa007e535289ee435df9ec4a92f3b229bbd9227
YJSoh <soh.you.jun@gmail.com>
2016-01-23 19:55:44 +0800

    Implement basic create admin with username / password only

--- app_server/app/controllers/AdminController.js
+++ app_server/app/controllers/AdminController.js
@@ -5,8 +5,10 @@
 var rfr = require('rfr');
 var Joi = require('joi');
 var Boom = require('boom');
+var bcrypt = require('bcryptjs');
 
 var Utility = rfr('app/util/Utility');
+var Authenticator = rfr('app/policies/Authenticator');
 var Service = rfr('app/services/Service');
 
 var logger = Utility.createLogger(__filename);
@@ -21,7 +23,7 @@ Class.registerRoutes = function () {
   this.server.route({
     method: 'POST', path: '/',
     config: {
-      auth: false
+      auth: {scope: Authenticator.SCOPE.ADMIN}
     },
     handler: this.createAdmin
   });
@@ -29,6 +31,27 @@ Class.registerRoutes = function () {
 
 /* Routes handlers */
 Class.createAdmin = function (request, reply) {
+  var credentials = {
+    username: request.payload.username,
+    password: encrypt(request.payload.password)
+  };
+
+  Service.createNewAdmin(credentials)
+  .then(function (admin) {
+    if (!admin || admin instanceof Error) {
+      return reply(Boom.badRequest('Unable to create admin ' + credentials));
+    }
+
+    // overwrite with unencrypted password
+    admin.password = request.payload.password;
+    reply(admin).created();
+  });
+};
+
+/* Helpers for everything above */
+var encrypt = function (password) {
+  var salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
 };
 
 exports.register = function (server, options, next) {
