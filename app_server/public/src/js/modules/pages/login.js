b2cb0050d4ac0024eed832eb3a34e2d46d126633
YJSoh <soh.you.jun@gmail.com>
2016-01-25 14:43:28 +0800

    Implement basic error message div

--- app_server/public/src/js/modules/pages/login.js
+++ app_server/public/src/js/modules/pages/login.js
@@ -9,31 +9,39 @@ const Login = module.exports = {
     let ctrl = this;
 
     ctrl.admin = new AdminModel();
+    ctrl.loginError = m.prop();
 
     ctrl.login = function (e) {
       e.preventDefault();
       AdminModel.login(ctrl.admin).then(function (admin) {
         // TODO: do something on success
       }, function (err) {
-        // TODO: do something on error
+        ctrl.loginError(err);
       });
     };
   },
   view: function (ctrl) {
     let admin = ctrl.admin;
 
-    return m('div.row', mz.text, [
-      m('form.col offset-s3 s6', {onsubmit: ctrl.login}, [
-        m('div.input-field col s12', [
-          m('input#username', {type: 'text', onchange: m.withAttr('value', admin.username)}),
-          m('label', {for: 'username'}, 'Username')
-        ]),
-        m('div.input-field col s12', [
-          m('input#password', {type: 'password', onchange: m.withAttr('value', admin.password)}),
-          m('label', {for: 'password'}, 'Password')
-        ]),
-        m('button.btn col s12', {type: 'submit'}, 'Log In')
+    let errorMessage = !ctrl.loginError() ? null :
+        m('div.row',
+            m('div.col s12 center-align message', ctrl.loginError().message));
+
+    return [
+      errorMessage,
+      m('div.row', mz.text, [
+        m('form.col offset-s3 s6', {onsubmit: ctrl.login}, [
+          m('div.input-field col s12', [
+            m('input#username', {type: 'text', onchange: m.withAttr('value', admin.username)}),
+            m('label', {for: 'username'}, 'Username')
+          ]),
+          m('div.input-field col s12', [
+            m('input#password', {type: 'password', onchange: m.withAttr('value', admin.password)}),
+            m('label', {for: 'password'}, 'Password')
+          ]),
+          m('button.btn col s12', {type: 'submit'}, 'Log In')
+        ])
       ])
-    ]);
+    ];
   }
 };
