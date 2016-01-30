ddae3fc92b9356ef82ac946ced3dda734b8b4e7d
kylelwm <kylelwm@gmail.com>
2016-01-30 15:53:47 +0800

    Set up SwipeRefreshLayout, hook up to ListView and setup dummy data into the list

--- client/android/WorldScope/app/src/main/java/com/litmus/worldscope/StreamRefreshListFragment.java
+++ client/android/WorldScope/app/src/main/java/com/litmus/worldscope/StreamRefreshListFragment.java
@@ -1,21 +1,42 @@
 package com.litmus.worldscope;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.litmus.worldscope.model.WorldScopeUser;
+import com.litmus.worldscope.model.WorldScopeViewStream;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class StreamRefreshListFragment extends Fragment {
 
+    private final String TAG = "StreamRefreshListFragment";
+
     /**
      * The fragment argument representing the section number for this
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private ListView listView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private WorldScopeStreamAdapter worldScopeStreamAdapter;
+    private ArrayList<WorldScopeViewStream> streams;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -35,8 +56,119 @@ public class StreamRefreshListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the fragment with the XML layout
         View rootView = inflater.inflate(R.layout.fragment_stream_list, container, false);
 
+        // Initiate the streams array
+        streams = new ArrayList<>();
+
+        // Set the WorldScopeStreamAdapter into ListView
+        swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipeRefreshLayout);
+        listView = (ListView) rootView.findViewById(R.id.streamListView);
+        worldScopeStreamAdapter = new WorldScopeStreamAdapter(getActivity(), R.layout.fragment_stream_list_item, streams);
+        listView.setAdapter(worldScopeStreamAdapter);
+
+        // Set the onRefreshListener into swipeRefreshLayout
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Adds one more dummy when refresh
+                putFakeData();
+                worldScopeStreamAdapter = new WorldScopeStreamAdapter(getActivity(), R.layout.fragment_stream_list_item, streams);
+                listView.setAdapter(worldScopeStreamAdapter);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        putFakeData();
+
         return rootView;
+    }
+
+    private void putFakeData() {
+        WorldScopeUser dummyUser = new WorldScopeUser();
+        dummyUser.setAlias("Ash Ketchum");
+
+        WorldScopeViewStream dummy = new WorldScopeViewStream();
+        dummy.setThumbnailLink("https://36.media.tumblr.com/0c71b9afd08d039c6c294578ba3c96e8/tumblr_mwhk5xPN8u1rgpyeqo1_500.png");
+        dummy.setTitle("Pikachu eating Pocky");
+        dummy.setOwner(dummyUser);
+        dummy.setCreatedAt(new Date());
+        dummy.setTotalViewers(123);
+
+        streams.add(dummy);
+    }
+
+    private class WorldScopeStreamAdapter extends ArrayAdapter<WorldScopeViewStream> {
+
+        private Context context;
+        private int layoutResourceId;
+        private ArrayList<WorldScopeViewStream> data;
+
+        public WorldScopeStreamAdapter(Context context, int layoutResourceId, ArrayList<WorldScopeViewStream> data) {
+            super(context, layoutResourceId, data);
+            this.context = context;
+            this.layoutResourceId = layoutResourceId;
+            this.data = data;
+        }
+
+        /**
+         * Override getView method in ArrayAdapter to configure how the view will look
+         */
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the item to display from data given position
+            WorldScopeViewStream stream = data.get(position);
+
+            /**
+             * Implement viewHolder pattern to cache the view to reduce calls to findViewById(),
+             * improving loading and response time
+             *
+             * ViewHolder patterns involve creating a class containing each individual elements that
+             * can be found in the XML layout and using it to 'hold' the view to reduce calls to
+             * findViewById()
+             *
+             * Check if convertView is null, if null then inflate and set viewHolder
+             */
+            ViewHolder viewHolder;
+            if(convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(R.layout.fragment_stream_list_item, parent, false);
+
+                viewHolder = new ViewHolder();
+                viewHolder.thumbnailImageView = (ImageView) convertView.findViewById(R.id.streamThumbnailImageView);
+                viewHolder.titleTextView = (TextView) convertView.findViewById(R.id.streamTitle);
+                viewHolder.ownerTextView = (TextView) convertView.findViewById(R.id.streamOwner);
+                viewHolder.createdAtTextView = (TextView) convertView.findViewById(R.id.startTime);
+                viewHolder.totalViewerTextView = (TextView) convertView.findViewById(R.id.numOfViewers);
+
+                // Access convertView's XML elements now using viewHolder
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            // Set text data into the view
+            viewHolder.titleTextView.setText(stream.getTitle());
+            viewHolder.ownerTextView.setText(stream.getOwner().getAlias());
+            viewHolder.createdAtTextView.setText(stream.getCreatedAt().toString());
+            viewHolder.totalViewerTextView.setText(String.valueOf(stream.getTotalViewers()));
+
+            // Use Picasso to set thumbnail image
+            Picasso.with(viewHolder.thumbnailImageView.getContext())
+                    .load(stream.getThumbnailLink())
+                    .into(viewHolder.thumbnailImageView);
+
+            return convertView;
+        }
+
+    }
+
+    private static class ViewHolder {
+        ImageView thumbnailImageView;
+        TextView titleTextView;
+        TextView ownerTextView;
+        TextView createdAtTextView;
+        TextView totalViewerTextView;
     }
 }
