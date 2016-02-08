f54985d8d4e087a88da08d77762aa9d036dd4b3a
kylelwm <kylelwm@gmail.com>
2016-02-08 23:15:05 +0800

    Added minimizing of app when back button is pressed in MainActivity

--- client/android/WorldScope/app/src/main/java/com/litmus/worldscope/MainActivity.java
+++ client/android/WorldScope/app/src/main/java/com/litmus/worldscope/MainActivity.java
@@ -19,6 +19,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -112,7 +113,10 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            Intent startMain = new Intent(Intent.ACTION_MAIN);
+            startMain.addCategory(Intent.CATEGORY_HOME);
+            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startMain);
         }
     }
 
