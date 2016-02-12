7a2645f3d4a1905fd9367206add1914a4f4a2656
YJSoh <soh.you.jun@gmail.com>
2016-02-12 17:39:31 +0800

    Make error display actually usable

---
+++ app_server/public/src/js/modules/components/errordisplay.js
@@ -0,0 +1,39 @@
+const m = require('mithril');
+
+const ErrorDisplay = module.exports = {
+  messages: m.prop([]),
+  addMessage: function (str) {
+    ErrorDisplay.messages().push(str);
+  },
+  setMessage: function (str) {
+    ErrorDisplay.resetMessages();
+    ErrorDisplay.addMessage(str);
+  },
+  removeMessageAt: function (idx) {
+    ErrorDisplay.messages().splice(idx, 1);
+    console.log(ErrorDisplay.messages());
+  },
+  resetMessages: function () {
+    ErrorDisplay.messages([]);
+  }
+};
+
+ErrorDisplay.controller = function () {
+  ErrorDisplay.resetMessages();
+};
+
+ErrorDisplay.view = function (ctrl) {
+  return m('div#error.row',
+      m('div.col s12 center-align',
+          ErrorDisplay.messages().map((msg, idx) =>
+            m('p', [
+              m('span', msg + ' '),
+              m('a.close', {
+                'data-index': idx,
+                onclick: m.withAttr('data-index', ErrorDisplay.removeMessageAt)
+              }, '(dismiss)')
+            ])
+          )
+      )
+  );
+};
