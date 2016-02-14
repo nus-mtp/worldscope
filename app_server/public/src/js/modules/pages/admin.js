8dc426cd66379eb5f19e9d4a76ee43493d3acf77
yj-soh <soh.you.jun@gmail.com>
2016-02-14 11:14:14 +0800

    Refactor, extract createPage and editPage

--- app_server/public/src/js/modules/pages/admin.js
+++ app_server/public/src/js/modules/pages/admin.js
@@ -4,69 +4,80 @@ const mz = require('../utils/mzInit');
 
 const AdminModel = require('../models/admin');
 
-const Admin = module.exports = {
-  controller: function () {
-    let username = m.route.param('username') || '';
+const Admin = module.exports = {};
 
-    let ctrl = this;
-
-    if (username) {
-      ctrl.title = 'Edit Admin';
-
-      ctrl.admin = m.prop();
-      AdminModel.get(username).then(ctrl.admin);
-
-      ctrl.action = function (e) {
-        e.preventDefault();
-        AdminModel.update(ctrl.admin()).then(
-            () => m.route('/admins'),
-            (err) => console.log(err) // TODO: display error
-        );
-      };
-    } else {
-      ctrl.title = 'Create Admin';
-      ctrl.admin = m.prop(new AdminModel());
-
-      ctrl.action = function (e) {
-        e.preventDefault();
-        AdminModel.create(ctrl.admin()).then(
-            () => m.route('/admins'),
-            (err) => console.log(err) // TODO: display error
-        );
-      };
-    }
+const createPage = {
+  title: 'Create Admin',
+  admin: m.prop(),
+  init: function () {
+    Admin.admin(new AdminModel());
   },
-  view: function (ctrl) {
-    let getLabelledInput = function (label, id, type, prop) {
-      let attributes = {
-        type: type,
-        value: prop(),
-        onchange: m.withAttr('value', prop)
-      };
+  action: function (e) {
+    e.preventDefault();
+    AdminModel.create(Admin.admin()).then(
+        () => m.route('/admins'),
+        (err) => console.log(err) // TODO: display error
+    );
+  }
+};
 
-      return [
-        m('input#' + id, attributes),
-        m('label.active', {for: id}, label)
-      ];
+const editPage = {
+  title: 'Edit Admin',
+  admin: m.prop(),
+  init: function () {
+    AdminModel.get(Admin.username).then(Admin.admin);
+  },
+  action: function (e) {
+    e.preventDefault();
+    AdminModel.update(Admin.admin()).then(
+        () => m.route('/admins'),
+        (err) => console.log(err) // TODO: display error
+    );
+  }
+};
+
+Admin.controller = function () {
+  Admin.username = m.route.param('username') || '';
+
+  if (Admin.username) {
+    Object.assign(Admin, editPage);
+  } else {
+    Object.assign(Admin, createPage);
+  }
+
+  Admin.init();
+};
+
+Admin.view = function () {
+  let getLabelledInput = function (label, id, type, prop) {
+    let attributes = {
+      type: type,
+      value: prop(),
+      onchange: m.withAttr('value', prop)
     };
 
-    let admin = ctrl.admin();
     return [
-      m('div.row', mz.text, [
-        m('h1', ctrl.title),
-        m('form.col s12', {onsubmit: ctrl.action}, [
-          m('div.input-field col s12',
-              getLabelledInput('Username', 'username', 'text', admin.username)
-          ),
-          m('div.input-field col s12',
-              getLabelledInput('Password', 'password', 'password', admin.password)
-          ),
-          m('div.input-field col s12',
-              getLabelledInput('Email', 'email', 'text', admin.email)
-          ),
-          m('button.btn col s12', {type: 'submit'}, ctrl.title)
-        ])
-      ])
+      m('input#' + id, attributes),
+      m('label', {for: id}, label)
     ];
-  }
+  };
+
+  let admin = Admin.admin();
+  return [
+    m('div.row', mz.text, [
+      m('h1', Admin.title),
+      m('form.col s12', {onsubmit: Admin.action}, [
+        m('div.input-field col s12',
+            getLabelledInput('Username', 'username', 'text', admin.username)
+        ),
+        m('div.input-field col s12',
+            getLabelledInput('Password', 'password', 'password', admin.password)
+        ),
+        m('div.input-field col s12',
+            getLabelledInput('Email', 'email', 'text', admin.email)
+        ),
+        m('button.btn col s12', {type: 'submit'}, Admin.title)
+      ])
+    ])
+  ];
 };
