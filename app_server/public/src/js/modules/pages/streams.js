f05842a044471e499da606dcc04102fab8ad8812
yj-soh <soh.you.jun@gmail.com>
2016-02-16 09:05:39 +0800

    Reorganize variables

--- app_server/public/src/js/modules/pages/streams.js
+++ app_server/public/src/js/modules/pages/streams.js
@@ -3,53 +3,55 @@ const m = require('mithril');
 const StreamModel = require('../models/stream');
 const DataDisplay = require('../components/datadisplay');
 
-const Streams = module.exports = {
-  controller: function () {
-    let ctrl = this;
+const Streams = module.exports = {};
 
-    ctrl.names = m.prop({
-      title: 'Title',
-      desc: 'Description',
-      stats: 'Statistics',
-      date: 'Start Date',
-      user: 'User',
-      actions: 'Actions'
-    });
-
-    let formatStats = (viewers, stickers) => [
-      m('span', viewers + 'V'),
-      m('br'),
-      m('span', stickers + 'S')
-    ];
-
-    let getActions = (id) =>
-        m('select', {onchange: m.withAttr('value', m.route)}, [
-          m('option', {disabled: true, selected: true}, 'Choose...'),
-          m('option', {value: '/streams/view/' + id}, 'View / Edit'),
-          m('option', {value: '/streams/stop/' + id}, 'Stop')
-        ]);
-
-    let parse = (streams) => streams.map(
-        function (stream) {
-          return {
-            title: stream.title(),
-            desc: stream.description(),
-            stats: formatStats(stream.viewers(), stream.stickers()),
-            date: stream.startDateTime(),
-            user: stream.user().alias(),
-            actions: getActions(stream.id())
-          };
-        }
-    );
-
-    ctrl.data = StreamModel.list().then(parse);
-  },
-  view: function (ctrl) {
-    return [m('h1', 'Streams'),
-      m(DataDisplay, {
-        names: ctrl.names(),
-        data: ctrl.data
-      })
-    ];
-  }
+Streams.init = function () {
+  Streams.streamData = StreamModel.list().then(parse);
 };
+
+const names = {
+  title: 'Title',
+  desc: 'Description',
+  stats: 'Statistics',
+  date: 'Start Date',
+  user: 'User',
+  actions: 'Actions'
+};
+
+const formatStats = (viewers, stickers) => [
+  m('span', viewers + 'V'),
+  m('br'),
+  m('span', stickers + 'S')
+];
+
+const getActions = (id) =>
+    m('select', {onchange: m.withAttr('value', m.route)}, [
+      m('option', {disabled: true, selected: true}, 'Choose...'),
+      m('option', {value: '/streams/view/' + id}, 'View / Edit'),
+      m('option', {value: '/streams/stop/' + id}, 'Stop')
+    ]);
+
+const parse = (streams) => streams.map(
+    function (stream) {
+      return {
+        title: stream.title(),
+        desc: stream.description(),
+        stats: formatStats(stream.viewers(), stream.stickers()),
+        date: stream.startDateTime(),
+        user: stream.user().alias(),
+        actions: getActions(stream.id())
+      };
+    }
+);
+
+Streams.controller = function () {
+  Streams.init();
+};
+
+Streams.view = () => [
+  m('h1', 'Streams'),
+  m(DataDisplay, {
+    names: names,
+    data: Streams.streamData
+  })
+];
