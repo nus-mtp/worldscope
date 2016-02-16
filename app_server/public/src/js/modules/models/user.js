18edf1ba0ccef21bc2e781fe8e3b7e7080768c11
yj-soh <soh.you.jun@gmail.com>
2016-02-16 08:19:56 +0800

    Defaults user alias to username

--- app_server/public/src/js/modules/models/user.js
+++ app_server/public/src/js/modules/models/user.js
@@ -5,7 +5,7 @@ const User = module.exports = function (data) {
   this.id = m.prop(data.userId);
   this.username = m.prop(data.username);
   this.platform = m.prop(data.platformType);
-  this.alias = m.prop(data.alias);
+  this.alias = m.prop(data.alias || data.username);
   this.description = m.prop(data.description);
   this.email = m.prop(data.email);
 };
