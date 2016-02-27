e89111ec4c98ee6517196de245ba529056eb4c50
yj-soh <soh.you.jun@gmail.com>
2016-02-27 08:36:57 +0800

    Add a prompt for datatable on no data found

--- app_server/public/src/js/modules/components/datatable.js
+++ app_server/public/src/js/modules/components/datatable.js
@@ -10,12 +10,14 @@ DataTable.view = function (ctrl, args) {
         columns.map((col) => m('th', args.names[col]))
       ])
     ]),
-    m('tbody', [
-      args.data.map(function (items) {
+    m('tbody',
+      args.data.length > 0 ? args.data.map(function (items) {
         return m('tr', [
           columns.map((col) => m('td', items[col]))
         ]);
-      })
-    ])
+      }) : m('td.center-align', {colspan: columns.length},
+          m('em', 'There\'s nothing found to show here!')
+      )
+    )
   ]);
 };
