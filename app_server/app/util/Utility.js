5722de5714311dc755f6240930bdb28aaeb8a7e2
yj-soh <soh.you.jun@gmail.com>
2016-02-29 05:41:13 +0800

    Add failAction function for expanding validation errors

--- app_server/app/util/Utility.js
+++ app_server/app/util/Utility.js
@@ -137,3 +137,20 @@ exports.changeToUnixTime = function(obj) {
 
   return obj;
 };
+
+var addValidationDetailsForJoi =
+/**
+ * Adds details for validation errors through validate's failAction property
+ * Assumes validation is done through joi
+ */
+exports.addValidationDetailsForJoi = function(request, reply, source, error) {
+  var errors = error.data.details.map(function(err) {
+    return {
+      param: err.path,
+      type: err.type
+    };
+  });
+  Object.assign(error.output.payload, {details: errors});
+
+  return reply(error);
+};
