a134980f9464909faf3d11e6725146447eb3fcf7
kylelwm <kylelwm@gmail.com>
2016-03-01 22:21:27 +0800

    Make WorldScopeCreatedStream model parcelable

--- client/android/WorldScope/app/src/main/java/com/litmus/worldscope/model/WorldScopeCreatedStream.java
+++ client/android/WorldScope/app/src/main/java/com/litmus/worldscope/model/WorldScopeCreatedStream.java
@@ -1,11 +1,12 @@
 package com.litmus.worldscope.model;
 
-import java.util.Date;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Stream object returned from WorldScope App Server when creating a new stream
  */
-public class WorldScopeCreatedStream extends WorldScopeStream {
+public class WorldScopeCreatedStream extends WorldScopeStream implements Parcelable {
 
     private long endedAt;
     private String streamLink;
@@ -15,4 +16,33 @@ public class WorldScopeCreatedStream extends WorldScopeStream {
 
     public void setEndedAt(long endedAt) {this.endedAt = endedAt;}
     public void setStreamLink(String streamLink) {this.streamLink = streamLink;}
+
+    protected WorldScopeCreatedStream(Parcel in) {
+        endedAt = in.readLong();
+        streamLink = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(endedAt);
+        dest.writeString(streamLink);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<WorldScopeCreatedStream> CREATOR = new Parcelable.Creator<WorldScopeCreatedStream>() {
+        @Override
+        public WorldScopeCreatedStream createFromParcel(Parcel in) {
+            return new WorldScopeCreatedStream(in);
+        }
+
+        @Override
+        public WorldScopeCreatedStream[] newArray(int size) {
+            return new WorldScopeCreatedStream[size];
+        }
+    };
 }
