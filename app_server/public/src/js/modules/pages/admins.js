6e06579ef5698d475c2c8436f1e16c500c184cd8
YJSoh <soh.you.jun@gmail.com>
2016-03-08 10:33:56 +0800

    Adjust admins and users to the same format as streams

--- app_server/public/src/js/modules/pages/admins.js
+++ app_server/public/src/js/modules/pages/admins.js
@@ -3,42 +3,45 @@ const m = require('mithril');
 const DataDisplay = require('../components/datadisplay');
 const AdminModel = require('../models/admin');
 
-const Admins = module.exports = {
-  controller: function () {
-    let ctrl = this;
+const Admins = module.exports = {};
 
-    ctrl.names = m.prop({
-      username: 'Username',
-      email: 'Email',
-      permissions: 'Permissions',
-      actions: 'Actions'
-    });
-
-    let getActions = (id) =>
-        m('select', {onchange: m.withAttr('value', m.route)}, [
-          m('option', {disabled: true, selected: true}, 'Choose...'),
-          m('option', {value: '/admins/view/' + id}, 'View / Edit'),
-          m('option', {value: '/admins/delete/' + id}, 'Delete')
-        ]);
-
-    let parse = (admins) => admins.map(
-        function (admin) {
-          return {
-            username: admin.username(),
-            email: admin.email(),
-            permissions: admin.permissions(),
-            actions: getActions(admin.username())
-          };
-        }
-    );
-
-    ctrl.data = AdminModel.list().then(parse);
-  },
-  view: (ctrl) => [
-    m('h1', 'Admins'),
-    m(DataDisplay, {
-      names: ctrl.names(),
-      data: ctrl.data
-    })
-  ]
+Admins.init = function () {
+  Admins.adminData = AdminModel.list().then(parse);
 };
+
+const names = {
+  username: 'Username',
+  email: 'Email',
+  permissions: 'Permissions',
+  actions: 'Actions'
+};
+
+const getActions = (id) =>
+  m('select', {onchange: m.withAttr('value', m.route)}, [
+    m('option', {disabled: true, selected: true}, 'Choose...'),
+    m('option', {value: '/admins/view/' + id}, 'View / Edit'),
+    m('option', {value: '/admins/delete/' + id}, 'Delete')
+  ]);
+
+const parse = (admins) => admins.map(
+    function (admin) {
+      return {
+        username: admin.username(),
+        email: admin.email(),
+        permissions: admin.permissions(),
+        actions: getActions(admin.username())
+      };
+    }
+);
+
+Admins.controller = function () {
+  Admins.init();
+};
+
+Admins.view = (ctrl) => [
+  m('h1', 'Admins'),
+  m(DataDisplay, {
+    names: names,
+    data: Admins.adminData
+  })
+];
--- app_server/public/src/js/modules/pages/users.js
+++ app_server/public/src/js/modules/pages/users.js
@@ -3,50 +3,53 @@ const m = require('mithril');
 const DataDisplay = require('../components/datadisplay');
 const UserModel = require('../models/user');
 
+const Users = module.exports = {};
+
+Users.init = function () {
+  Users.userData = UserModel.list().then(parse);
+};
+
 const platformImg = {
   facebook: '/admin/img/facebook.png'
 };
 
-const Users = module.exports = {
-  controller: function () {
-    let ctrl = this;
-
-    ctrl.names = m.prop({
-      id: 'Id',
-      name: 'Name',
-      subscribers: 'Subscribers',
-      platform: 'Platform',
-      actions: 'Actions'
-    });
-
-    let getPlatform = (platform) => m('img', {src: platformImg[platform]});
-
-    let getActions = (id) => [
-      m('select', {onchange: m.withAttr('value', m.route)}, [
-        m('option', {disabled: true, selected: true}, 'Choose...'),
-        m('option', {value: '/users/view/' + id}, 'View / Edit')
-      ])
-    ];
-
-    let parse = (users) => users.map(
-        function (user) {
-          return {
-            id: user.id(),
-            name: user.alias(),
-            subscribers: '42', // TODO: Get user's subscriber count
-            platform: getPlatform(user.platform()),
-            actions: getActions(user.id())
-          };
-        }
-    );
-
-    ctrl.data = UserModel.list().then(parse);
-  },
-  view: (ctrl) => [
-    m('h1', 'Users'),
-    m(DataDisplay, {
-      names: ctrl.names(),
-      data: ctrl.data
-    })
-  ]
+const names = {
+  id: 'Id',
+  name: 'Name',
+  subscribers: 'Subscribers',
+  platform: 'Platform',
+  actions: 'Actions'
 };
+
+const getPlatformImg = (platform) => m('img', {src: platformImg[platform]});
+
+const getActions = (id) => [
+  m('select', {onchange: m.withAttr('value', m.route)}, [
+    m('option', {disabled: true, selected: true}, 'Choose...'),
+    m('option', {value: '/users/view/' + id}, 'View / Edit')
+  ])
+];
+
+const parse = (users) => users.map(
+    function (user) {
+      return {
+        id: user.id(),
+        name: user.alias(),
+        subscribers: '42', // TODO: Get user's subscriber count
+        platform: getPlatformImg(user.platform()),
+        actions: getActions(user.id())
+      };
+    }
+);
+
+Users.controller = function () {
+  Users.init();
+};
+
+Users.view = () => [
+  m('h1', 'Users'),
+  m(DataDisplay, {
+    names: names,
+    data: Users.userData
+  })
+];
