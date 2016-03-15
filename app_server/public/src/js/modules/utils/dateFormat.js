20d3bcd3e4c9e32d3731a07194823a4d0cf2afba
YJSoh <soh.you.jun@gmail.com>
2016-03-15 10:00:36 +0800

    Fix dates to show with leading zero

--- app_server/public/src/js/modules/utils/dateFormat.js
+++ app_server/public/src/js/modules/utils/dateFormat.js
@@ -2,9 +2,10 @@
  * 'timeFormat' formats time into more readable formats.
  */
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const to2Digits = (i) => i < 10 ? '0' + i : i;
 const timeFormat = module.exports = {
   toShortDateTime: function (d) {
-    return d.getDay() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear() + ', ' +
-        d.getHours() + ':' + d.getMinutes();
+    return to2Digits(d.getDay()) + ' ' + months[d.getMonth()] + ' ' + d.getFullYear() + ', ' +
+        to2Digits(d.getHours()) + ':' + to2Digits(d.getMinutes());
   }
 };
