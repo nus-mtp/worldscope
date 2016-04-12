41770e12e0780b57ad80a48c2a4cd07f57aadfa6
Keith Leong Wei Mun <Keith Leong Wei Mun>
2016-04-13 03:44:38 +0800

    Implemented automated UI testing for Android

--- client/android/WorldScope/app/src/androidTest/java/com/litmus/worldscope/LoginActivityTest.java
+++ client/android/WorldScope/app/src/androidTest/java/com/litmus/worldscope/LoginActivityTest.java
@@ -5,8 +5,6 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.litmus.worldscope.model.WorldScopeUser;
-
 import junit.framework.TestCase;
 
 import org.junit.Before;
